18d297f678de01034126f5d0c832312df9dba90c
Dax Allen <dax.e.allen@gmail.com>
2019-07-08 19:10:43 -0600

    small refactor on labcoin

--- labCoin/Coin.java
+++ labCoin/Coin.java
@@ -2,7 +2,10 @@ package labCoin;
 
 public enum Coin {
 	
-	CENT(2.5, 19.05), NICKEL(5.0, 21.21), DIME(2.268, 17.91), QUARTER(5.670, 24.26);
+	CENT(2.5, 19.05), 
+	NICKEL(5.0, 21.21), 
+	DIME(2.268, 17.91), 
+	QUARTER(5.670, 24.26);
 	
 	private final double weight;
 	private final double diameter;
